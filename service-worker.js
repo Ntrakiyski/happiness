eaa241bc80e509c49e61607c00bfff569d3bab6e
Ntrakiyski <62264455+Ntrakiyski@users.noreply.github.com>
2020-03-28 22:19:46 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,28 @@
+const cacheName = 'V1';
+
+const cacheAssets = [
+'index.html',
+'past.html',
+'present.html',
+'future.html',
+'style.css',
+'main.js'
+]; 
+
+
 self.addEventListener('install', function(event){
   console.log('[Service Worker] Installing Service Worker.....',event);
+
+  e.waitUntil(
+  	caches
+  	.open(cacheName)
+  	.then(cache => {
+  		console.log('Service worker: Caching files');
+  		cache.addAll(cacheAssets);
+  	})
+  	.then(() => self.skipWaiting() )
+  	);
+
 });
 
 self.addEventListener('activate', function(event){
