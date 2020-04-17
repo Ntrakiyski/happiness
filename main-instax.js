752f8c3dfc4be0adf1852dee1a79cab889bae139
Ntrakiyski <62264455+Ntrakiyski@users.noreply.github.com>
2020-04-17 23:59:59 +0300

    Add files via upload

---
+++ main-instax.js
@@ -0,0 +1,15 @@
+const element = document.querySelector("expanded-image-section")
+
+document.getElementById('firstimage-section').addEventListener('click',function(){
+
+  document.querySelector('.expanded-image-section').style.display = 'block';
+
+  });
+document.getElementById('white-line-position').addEventListener('click',function(){
+
+
+  
+  document.querySelector('.expanded-image-section').style.display = 'none';
+   
+
+  });
