66ba6112d410c3373b3d3258e9d189ca6d0381a7
Ntrakiyski <62264455+Ntrakiyski@users.noreply.github.com>
2020-04-18 16:30:19 +0300

    Add files via upload

--- main-instax.js
+++ main-instax.js
@@ -1,8 +1,8 @@
-const element = document.querySelector("expanded-image-section")
+/*const element = document.querySelector("expanded-image-section")
 
 document.getElementById('firstimage-section').addEventListener('click',function(){
 
-  document.querySelector('.expanded-image-section').style.display = 'block';
+  document.querySelector('.expanded-image-section').style.visability = 'visible';
 
   });
 document.getElementById('white-line-position').addEventListener('click',function(){
@@ -12,4 +12,18 @@ document.getElementById('white-line-position').addEventListener('click',function
   document.querySelector('.expanded-image-section').style.display = 'none';
    
 
-  });
+  }); */
+
+  function myFunction() {
+   var element = document.getElementById("expanded-image-section");
+   element.classList.toggle("show");
+   element.classList.remove("hidden");
+}
+function myFunction2() {
+   var element = document.getElementById("expanded-image-section");
+   element.classList.toggle("hidden");
+   element.classList.remove("show");
+}
+
+
+
