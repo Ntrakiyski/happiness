06ef07d1a4983643a6dbfa8612d36e711a9e1ee4
Ntrakiyski <62264455+Ntrakiyski@users.noreply.github.com>
2020-04-19 11:40:08 +0300

    Add files via upload

--- main-musicplayer.js
+++ main-musicplayer.js
@@ -1,3 +1,62 @@
-function changeDivImage() {
-    $("body").toggleClass("on");
+var isColored = false;
+document.getElementById('settings-icon').addEventListener("click", function(){
+
+if(isColored){
+   document.querySelector('body').style.background = '#181C22';
+  document.querySelector('.back-arrow').style.background = '#181C22';
+  document.querySelector('.settings-icon').style.background = '#181C22';
+  document.querySelector('.playimage').style.background = '#181C22';
+  document.querySelector('.playimage2').style.background = '#181C22';
+  document.querySelector('.playimage3').style.background = '#181C22';
+  document.querySelector('.playimage4').style.background = '#181C22';
+  document.querySelector('.playimage5').style.background = '#181C22';
+  document.querySelector('.preview-anounce').style.color = '#fff';
+  document.querySelector('.main-title').style.color = '#fff';
+  document.querySelector('.preview-title').style.color = '#fff';
+  document.querySelector('.preview-image').style.opacity = '1';
+isColored = false;
+} else{
+  document.querySelector('body').style.background = '#fff';
+  document.querySelector('.back-arrow').style.background = '#fff';
+  document.querySelector('.settings-icon').style.background = '#fff';
+  document.querySelector('.playimage').style.background = '#fff';
+  document.querySelector('.playimage2').style.background = '#fff';
+  document.querySelector('.playimage3').style.background = '#fff';
+  document.querySelector('.playimage4').style.background = '#fff';
+  document.querySelector('.playimage5').style.background = '#fff';
+  document.querySelector('.preview-anounce').style.color = '#A6A5AF';
+  document.querySelector('.main-title').style.color = '#181C22';
+  document.querySelector('.preview-title').style.color = '#181C22';
+  document.querySelector('.preview-image').style.opacity = '0.9';
+isColored = true;
 }
+
+
+});
+
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("moretextlink1");
+  var moreT = document.getElementById("moreT");
+   var moreText2 = document.getElementById("moretextlink2");
+  
+
+function readmoreFunction() {
+
+    dots.style.display = "none";
+    moreText.style.display = "none";
+  	moreT.style.display = "inline";
+    moreText2.style.display = "inline";
+  
+
+
+};
+
+function readlessFunction() {
+
+    dots.style.display = "inline";
+    moreText.style.display = "inline";
+  	moreT.style.display = "none";
+    moreText2.style.display = "none";
+ 
+}
+
